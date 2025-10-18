ec09e8bd98b68448ce9452d2a62f05a6059617a0
Hebert Rocha <hebertrocha76@gmail.com>
2025-10-17 22:20:20 -0300

    Creating an exceptions filter

---
+++ ProductClientHub.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ProductClientHub.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            
+        }
+    }
+}
