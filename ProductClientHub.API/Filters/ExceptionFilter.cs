721d6020f38363ba15307e4a602940a347b50de0
Hebert Rocha <hebertrocha76@gmail.com>
2025-10-17 22:20:20 -0300

    Creating an exceptions filter

--- ProductClientHub.API/Filters/ExceptionFilter.cs
+++ ProductClientHub.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Exceptions.ExceptionsBase;
 
 namespace ProductClientHub.API.Filters
 {
@@ -6,7 +9,22 @@ namespace ProductClientHub.API.Filters
     {
         public void OnException(ExceptionContext context)
         {
-            
+            if (context.Exception is ProductClientHubException productClientHubException)
+            {
+                context.HttpContext.Response.StatusCode = (int)productClientHubException.GetHttpStatusCode();
+
+                context.Result = new ObjectResult(new ResponseErrorMessagesJson(productClientHubException.GetErrors()));
+            } 
+            else
+            {
+                ThrowUnknownError(context);
+            }
+        }
+
+        private void ThrowUnknownError(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorMessagesJson("Erro Desconhecido."));
         }
     }
 }
