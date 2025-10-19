82338ec451e28454b319e8cf0ace565c43cfd502
Hebert Rocha <hebertrocha76@gmail.com>
2025-10-19 13:01:18 -0300

    Configuring Entity Framework in the project

---
+++ ProductClientHub.API/Infrastructure/PorductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class PorductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; } = default!;
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\heber\\OneDrive\\Documentos\\Cursos\\Programação\\rocketseat\\c# curso\\ProductClientHub.db");
+        }
+    }
+}
